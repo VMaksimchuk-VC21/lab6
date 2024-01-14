c4fbefa3ce425a3d7ed2908bd14191641cbdaf77
Валерия Максимчук <VMaksimchuk-VC21@stud.sfu-kras.ru>
2024-01-15 01:03:50 +0700

    information

--- main.py
+++ main.py
@@ -1,80 +1,99 @@
 class TransportCompany:
     def __init__(self, name):
+        """Инициализация атрибутов класса"""
         self.__name = name
         self.__vehicles = []
 
     @property
     def name(self):
+        """Уровень доступности атрибута"""
         return self.__name
 
     @property
     def vehicles(self):
+        """Уровень доступности атрибута"""
         return self.__vehicles
 
     def __str__(self):
+        """Вывод информации об объекте"""
         return f"Фирма: {self.name}"
 
     def add_vehicle(self, vehicle):
+        """Добавляет новый транспорт в список."""
         self.__vehicles.append(vehicle)
 
 
 class Vehicle:
     def __init__(self, model, company):
+        """Инициализация атрибутов класса"""
         self.__model = model
         self.__company = company
         self.__drivers = []
 
     @property
     def model(self):
+        """Уровень доступности атрибута"""
         return self.__model
 
     @property
     def company(self):
+        """Уровень доступности атрибута"""
         return self.__company
 
     @property
     def drivers(self):
+        """Уровень доступности атрибута"""
         return self.__drivers
 
     def __str__(self):
+        """Вывод информации об объекте"""
         return f"Транспорт: {self.model} ({self.company.name})"
 
     def add_driver(self, driver):
+        """Добавляет нового водителя в список."""
         self.__drivers.append(driver)
 
 
 class Driver:
     def __init__(self, name):
+        """Инициализация атрибутов класса"""
         self.__name = name
 
     @property
     def name(self):
+        """Уровень доступности атрибута"""
         return self.__name
 
     def __str__(self):
+        """Вывод информации об объекте"""
         return f"Водитель: {self.name}"
 
 
 def create_company():
+    """Создает компанию, запрашивая у пользователя название."""
     name = input("Введите название фирмы: ")
     return TransportCompany(name)
 
 
 def create_vehicle(company):
+    """Создает транспорт, запрашивая у пользователя его модель."""
     model = input("Введите модель транспорта: ")
     return Vehicle(model, company)
 
 
 def create_driver():
+    """Создает водителя, запрашивая у пользователя его имя."""
     name = input("Введите имя водителя: ")
     return Driver(name)
 
 
 def print_driver(driver):
+    """Выводим имя водителя"""
     print(driver)
 
 
 def menu():
+    """Отображает меню и предоставляет пользователю опции для взаимодействия с программой."""
     company = None
     vehicle = None
     driver = None
